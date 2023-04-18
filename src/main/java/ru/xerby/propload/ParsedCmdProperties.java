82ab4542655057266cc683ba6f5b8cb105055612
Xerby <Xerby85@gmail.com>
2023-04-18 23:46:19 +0200

    Is improved ParsedCmdProperties and added tests

--- src/main/java/ru/xerby/propload/ParsedCmdProperties.java
+++ src/main/java/ru/xerby/propload/ParsedCmdProperties.java
@@ -1,13 +1,75 @@
 package ru.xerby.propload;
 
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
+@EqualsAndHashCode
 public class ParsedCmdProperties {
     private final List<ParsedCmdProperty> properties = new ArrayList<>();
 
+    @SuppressWarnings("java:S3776")
+    public static ParsedCmdProperties parse(String[] args, boolean isWindows, boolean throwExceptionIfUnboundTokenFound) {
+        String propname = null;
+        String propval = null;
+        boolean lastSymbolIsKey = false;
+        ParsedCmdProperties res = new ParsedCmdProperties();
+        if (args == null) return res;
+        for (String key : args) {
+            if (key.strip().startsWith("/") && isWindows && key.strip().length() > 1) {
+                if (propname != null) {
+                    res.add(propname, propval, false);
+                }
+                propname = key.strip().substring(1);
+                lastSymbolIsKey = true;
+            } else if (key.strip().startsWith("--") && key.strip().length() > 2) {
+                if (propname != null) {
+                    res.add(propname, propval, false);
+                }
+                propname = key.strip().substring(2);
+                lastSymbolIsKey = true;
+            } else if (lastSymbolIsKey) {
+                propval = key.strip();
+                lastSymbolIsKey = false;
+                res.add(propname, propval, false);
+                propname = null;
+                propval = null;
+            } else {
+                if (throwExceptionIfUnboundTokenFound)
+                    throw new RuntimeException("Unbound token found: " + key + " in " + Arrays.toString(args).replace(", ", " "));
+                else
+                    log.debug("Unbound token found: " + key + "\n in " + Arrays.toString(args).replace(", ", " "));
+            }
+
+            if (propname != null && propname.contains("=")) {
+                propval = propname.substring(propname.indexOf("=") + 1).strip();
+                propname = propname.substring(0, propname.indexOf("=")).strip();
+
+                lastSymbolIsKey = false;
+                res.add(propname, propval, true);
+                propname = null;
+                propval = null;
+            }
+        }
+        if (propname != null) {
+            res.add(propname, propval, false);
+        }
+        return res;
+    }
+
+    public void add(String key) {
+        add(key, null, false);
+    }
+
     public void add(String key, String value, boolean isSurelyParametrized) {
+        if (key == null)
+            throw new RuntimeException("Key can't be null");
+        if (value == null && isSurelyParametrized)
+            throw new RuntimeException("Value can't be null if isSurelyParametrized is true");
         properties.add(new ParsedCmdProperty(key, value, isSurelyParametrized));
     }
 
@@ -55,67 +117,17 @@ public class ParsedCmdProperties {
         properties.clear();
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         for (ParsedCmdProperty parsedCmdProperty : properties) {
             sb.append("--").append(parsedCmdProperty.getKey());
             if (parsedCmdProperty.isSurelyParametrized()) {
                 sb.append("=").append(parsedCmdProperty.getValue());
-            }
-            else if (parsedCmdProperty.getValue() != null) {
+            } else if (parsedCmdProperty.getValue() != null) {
                 sb.append(" ").append(parsedCmdProperty.getValue());
             }
             sb.append(" ");
         }
         return sb.toString();
     }
-
-    public static ParsedCmdProperties parse(String[] args, boolean isWindows, boolean throwExceptionIfUnboundTokenFound) {
-        String propname = null;
-        String propval = null;
-        boolean lastSymbolIsKey = false;
-        ParsedCmdProperties res = new ParsedCmdProperties();
-        for (String key : args) {
-            if (key.strip().startsWith("/") && isWindows && key.strip().length() > 1) {
-                if (propname != null) {
-                    res.add(propname, propval, false);
-                }
-                propname = key.strip().substring(1);
-                lastSymbolIsKey = true;
-            } else if (key.strip().startsWith("--") && key.strip().length() > 2) {
-                if (propname != null) {
-                    res.add(propname, propval, false);
-                }
-                propname = key.strip().substring(2);
-                lastSymbolIsKey = true;
-            } else if (lastSymbolIsKey) {
-                propval = key.strip();
-                lastSymbolIsKey = false;
-                res.add(propname, propval, false);
-                propname = null;
-                propval = null;
-            } else {
-                if (throwExceptionIfUnboundTokenFound)
-                    throw new RuntimeException("Unbound token found: " + key+" in "+ Arrays.toString(args).replace(", ", " "));
-                else
-                    System.err.println("Unbound token found: " + key+"\n in "+Arrays.toString(args).replace(", ", " "));
-            }
-
-            if (propname != null && propname.contains("=")) {
-                propval = propname.substring(propname.indexOf("=")+1).strip();
-                propname = propname.substring(0, propname.indexOf("=")).strip();
-
-                lastSymbolIsKey = false;
-                res.add(propname, propval, true);
-                propname = null;
-                propval = null;
-            }
-        }
-        if (propname != null) {
-            res.add(propname, propval, false);
-        }
-        return res;
-    }
-
-
 }
---
+++ src/test/java/ru/xerby/propload/CmdParserTest.java
@@ -0,0 +1,214 @@
+package ru.xerby.propload;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CmdParserTest {
+
+    @Test
+    public void testEmpty() {
+        var props = new ParsedCmdProperties();
+        Assert.assertEquals("If args is null method must return empty ParsedCmdProperties",
+                props,
+                ParsedCmdProperties.parse(null, true, false));
+
+        props = new ParsedCmdProperties();
+        Assert.assertEquals("If args is empty array method must return empty ParsedCmdProperties",
+                props,
+                ParsedCmdProperties.parse(new String[]{}, true, false));
+    }
+
+    @Test
+    public void testSimpleProperty() {
+        var props = new ParsedCmdProperties();
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", null, false);
+        Assert.assertEquals("One simple key",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", null, false);
+        Assert.assertEquals("Windows style simple key with windows parse mode",
+                props,
+                ParsedCmdProperties.parse(new String[]{"/delayed"}, true, false));
+
+        props = new ParsedCmdProperties();
+        Assert.assertEquals("Windows style simple key with non-windows parse mode",
+                props,
+                ParsedCmdProperties.parse(new String[]{"/delayed"}, false, false));
+    }
+
+    @Test
+    public void testParametrizedProperty() {
+        var props = new ParsedCmdProperties();
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", false);
+        Assert.assertEquals("Check that not surely parametrized property is treated as not surely parametrized",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "5min"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", true);
+        Assert.assertNotEquals("Check that not surely parametrized property is not equal surely parametrized property with the same key and value",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "5min"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", true);
+        Assert.assertEquals("Check that surely parametrized property is treated as surely parametrized",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed=5min"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", false);
+        Assert.assertNotEquals("Check that surely parametrized property is not equal not surely parametrized property with the same key and value",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed=5min"}, true, false));
+    }
+
+
+    @Test
+    public void testFewProperties() {
+        var props = new ParsedCmdProperties();
+
+        props = new ParsedCmdProperties();
+        props.add("delayed");
+        props.add("mail");
+        Assert.assertEquals("Check a parameterless property followed by other property",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "--mail"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", false);
+        props.add("mail");
+        Assert.assertEquals("Check a parametrized property followed by other property",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "5min", "--mail"}, true, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "5min", true);
+        props.add("mail");
+        Assert.assertEquals("Check a surely parametrized property parameter followed by other property",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed=5min", "--mail"}, true, false));
+    }
+
+    @Test
+    public void testWindowsKeys() {
+        var props = new ParsedCmdProperties();
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "/5min", false);
+        Assert.assertEquals("Check that in non-windows mode, token that started with \"/\" treated as value of previous parameter",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "/5min"}, false, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed");
+        props.add("5min");
+        Assert.assertEquals("Check that in windows mode, token that started with \"/\" treated as parameter key",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "/5min"}, true, false));
+    }
+
+    @Test
+    public void testRedundantToken() {
+        var props = new ParsedCmdProperties();
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "/5min", false);
+        //todo:need to check logs
+        Assert.assertEquals("If throwExceptionIfUnboundTokenFound is false, then redundant token just omit and is mentioned in log",
+                props,
+                ParsedCmdProperties.parse(new String[]{"--delayed", "/5min", "/mail"}, false, false));
+
+        props = new ParsedCmdProperties();
+        props.add("delayed", "/5min", false);
+        Assert.assertThrows("If throwExceptionIfUnboundTokenFound is true, then if is there  redundant token then exception is thrown",
+                RuntimeException.class,
+                () -> ParsedCmdProperties.parse(new String[]{"--delayed", "/5min", "/mail"}, false, true));
+
+        System.out.println(ParsedCmdProperties.parse(new String[]{"/delayed", "--no-ops", "pzdariki", "gigi", "/commando", "opa", "--size=plpl"}, true, false));
+    }
+
+    @Test
+    public void testEmptyAndClear() {
+        var props = new ParsedCmdProperties();
+
+        Assert.assertTrue("Property is empty after creation", props.isEmpty());
+
+        props.add("delayed", "5min", false);
+        props.add("mail");
+        props.add("no-ops", "false", true);
+
+        Assert.assertFalse("Property is not empty after adding", props.isEmpty());
+        props.clear();
+        Assert.assertTrue("Property is empty after clearing", props.isEmpty());
+    }
+
+    @Test
+    public void testGetMethods() {
+        var props = new ParsedCmdProperties();
+
+        props.add("delayed", "5min", false);
+        props.add("mail");
+        props.add("no-ops", "false", true);
+
+        Assert.assertTrue("Can find all 3 keys", props.containsKey("delayed") && props.containsKey("mail") && props.containsKey("no-ops"));
+        Assert.assertFalse("Can't find all non-existent key", props.containsKey("govern"));
+
+        Assert.assertEquals("Check value for the first property", "5min", props.getValue("delayed"));
+        Assert.assertNull("Value of mail is null", props.getValue("mail"));
+        Assert.assertEquals("Check value for the second property", "false", props.getValue("no-ops"));
+        Assert.assertNull("Value of non-existent is null", props.getValue("govern"));
+
+        Assert.assertFalse("Check if the first property surely", props.isSurelyParametrized("delayed"));
+        Assert.assertFalse("Check if the second property surely", props.isSurelyParametrized("mail"));
+        Assert.assertTrue("Check if the third property surely", props.isSurelyParametrized("no-ops"));
+        Assert.assertFalse("Check non-existent property surely", props.isSurelyParametrized("govern"));
+    }
+
+
+    @Test
+    public void testMalformedAddProperty() {
+        var props = new ParsedCmdProperties();
+
+        Assert.assertThrows("Can't add property without name",
+                RuntimeException.class,
+                () -> props.add(null, "5min", false));
+
+        Assert.assertThrows("Can't add property without parameter value with isSurelyParameterized=true",
+                RuntimeException.class,
+                () -> props.add("no-ops", null, true));
+    }
+
+
+    @Test
+    public void testGetProperty() {
+        var props = new ParsedCmdProperties();
+
+        props.add("delayed", "5min", false);
+        props.add("mail");
+        props.add("no-ops", "false", true);
+
+        Assert.assertEquals("Can find all 3 keys",
+                new ParsedCmdProperty("delayed", "5min", false),
+                props.getParsedCmdProperty("delayed"));
+        Assert.assertNotEquals("Can find all 3 keys",
+                new ParsedCmdProperty("delayed", "2min", false),
+                props.getParsedCmdProperty("delayed"));
+        Assert.assertNotEquals("Can find all 3 keys",
+                new ParsedCmdProperty("delayed", "5min", true),
+                props.getParsedCmdProperty("delayed"));
+        Assert.assertNotEquals("Can find all 3 keys",
+                new ParsedCmdProperty("delay", "5min", true),
+                props.getParsedCmdProperty("delayed"));
+        Assert.assertNull("Can't find all non-existent key", props.getParsedCmdProperty("dead"));
+    }
+
+
+}
